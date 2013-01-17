76f4e37ef2bdd3f14d779fdb06ca6c2ab5c06fbb
Willian Fernandes <willian@willianfernandes.com.br>
2013-01-17 13:56:23 -0200

    Added an installation script.

---
+++ install.sh
@@ -0,0 +1,16 @@
+CURRENT_PATH=`pwd`
+
+# Linking
+ln -sf $CURRENT_PATH/.bash $HOME/.bash
+ln -sf $CURRENT_PATH/.bash_profile $HOME/.bash_profile
+ln -sf $CURRENT_PATH/.gemrc $HOME/.gemrc
+ln -sf $CURRENT_PATH/.gitconfig $HOME/.gitconfig
+ln -sf $CURRENT_PATH/.inputrc $HOME/.inputrc
+ln -sf $CURRENT_PATH/.irbrc $HOME/.irbrc
+ln -sf $CURRENT_PATH/.npmrc $HOME/.npmrc
+ln -sf $CURRENT_PATH/.pryrc $HOME/.pryrc
+
+source $HOME/.bash_profile
+
+echo "[OK]"
+
