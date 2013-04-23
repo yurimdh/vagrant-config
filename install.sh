fe37f856885efc4598c2f15e4cf53a75d74b47d5
Willian Fernandes <willian@willianfernandes.com.br>
2013-04-23 16:10:59 -0300

    Added .caprc and .gitignore files

--- install.sh
+++ install.sh
@@ -4,8 +4,10 @@ CURRENT_PATH=`pwd`
 ln -sf $CURRENT_PATH/.ackrc $HOME/.ackrc
 ln -sf $CURRENT_PATH/.bash $HOME/.bash
 ln -sf $CURRENT_PATH/.bash_profile $HOME/.bash_profile
+ln -sf $CURRENT_PATH/.caprc $HOME/.caprc
 ln -sf $CURRENT_PATH/.gemrc $HOME/.gemrc
 ln -sf $CURRENT_PATH/.gitconfig $HOME/.gitconfig
+ln -sf $CURRENT_PATH/.gitignore $HOME/.gitignore
 ln -sf $CURRENT_PATH/.inputrc $HOME/.inputrc
 ln -sf $CURRENT_PATH/.irbrc $HOME/.irbrc
 ln -sf $CURRENT_PATH/.npmrc $HOME/.npmrc
@@ -14,4 +16,3 @@ ln -sf $CURRENT_PATH/.pryrc $HOME/.pryrc
 source $HOME/.bash_profile
 
 echo "[OK]"
-
